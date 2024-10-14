eae6bef11c7f2f6086b82a3b179eb84c53c5adb7
sujoysenmyself <sujoysenjitu1994@gmail.com>
2024-10-14 11:43:43 +0530

    code modified

--- header-footer-insertion.php
+++ header-footer-insertion.php
@@ -1,33 +1,41 @@
 <?php
 /*
 Plugin Name: Header Footer Insertion
-Plugin URI: https://wordpress.org/plugins/header-and-footer-insertion/
+Plugin URI: https://wordpress.org/plugins/header-footer-insertion/
 Description: A plugin to add custom styles and scripts to the header or footer.
-Version: 1.0.0
+Version: 1.0.1
 Requires at least: 5.8
 Requires PHP: 7.4
 Author: Sujoy Sen
 Author URI: https://sujoysenmyself.github.io/myresume/
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: HFI
+Text Domain: hfinsert
 */
 
 // Add menu page
-add_action('admin_menu', 'HFI_add_admin_menu');
-function HFI_add_admin_menu() {
-    add_menu_page('Header and Footer', 'Header and Footer', 'manage_options', 'HFI', 'HFI_create_admin_page', 'dashicons-editor-code', 26);
+add_action('admin_menu', 'hfinsert_add_admin_menu');
+function hfinsert_add_admin_menu() {
+    add_menu_page(
+        'Header and Footer', 
+        'Header and Footer', 
+        'manage_options', 
+        'hfinsert', 
+        'hfinsert_create_admin_page', 
+        'dashicons-editor-code', 
+        26
+    );
 }
 
 // Create admin page
-function HFI_create_admin_page() {
+function hfinsert_create_admin_page() {
     ?>
     <div class="wrap">
         <h1>Insert Header and Footer</h1>
         <form method="post" action="options.php">
             <?php
-            settings_fields('HFI_settings_group');
-            do_settings_sections('HFI');
+            settings_fields('hfinsert_settings_group');
+            do_settings_sections('hfinsert');
             submit_button();
             ?>
         </form>
@@ -36,50 +44,84 @@ function HFI_create_admin_page() {
 }
 
 // Register settings
-add_action('admin_init', 'HFI_register_settings');
-function HFI_register_settings() {
-    register_setting('HFI_settings_group', 'HFI_header_css');
-    register_setting('HFI_settings_group', 'HFI_header_js');
-    register_setting('HFI_settings_group', 'HFI_footer_js');
+add_action('admin_init', 'hfinsert_register_settings');
+function hfinsert_register_settings() {
+    register_setting('hfinsert_settings_group', 'hfinsert_header_css');
+    register_setting('hfinsert_settings_group', 'hfinsert_header_js');
+    register_setting('hfinsert_settings_group', 'hfinsert_footer_js');
 
-    add_settings_section('HFI_settings_section', null, null, 'HFI');
+    add_settings_section('hfinsert_settings_section', null, null, 'hfinsert');
 
-    add_settings_field('HFI_header_css', 'CSS in Header', 'HFI_header_css_callback', 'HFI', 'HFI_settings_section');
-    add_settings_field('HFI_header_js', 'JS in Header', 'HFI_header_js_callback', 'HFI', 'HFI_settings_section');
-    add_settings_field('HFI_footer_js', 'JS in Footer', 'HFI_footer_js_callback', 'HFI', 'HFI_settings_section');
+    add_settings_field(
+        'hfinsert_header_css', 
+        'CSS in Header', 
+        'hfinsert_header_css_callback', 
+        'hfinsert', 
+        'hfinsert_settings_section'
+    );
+    add_settings_field(
+        'hfinsert_header_js', 
+        'JS in Header', 
+        'hfinsert_header_js_callback', 
+        'hfinsert', 
+        'hfinsert_settings_section'
+    );
+    add_settings_field(
+        'hfinsert_footer_js', 
+        'JS in Footer', 
+        'hfinsert_footer_js_callback', 
+        'hfinsert', 
+        'hfinsert_settings_section'
+    );
 }
 
 // Callbacks for input fields
-function HFI_header_css_callback() {
-    $header_css = esc_textarea(get_option('HFI_header_css'));
-    echo "<textarea name='HFI_header_css' rows='5' cols='50' style='width: 100%;'>$header_css</textarea>";
+function hfinsert_header_css_callback() {
+    $header_css = esc_textarea(get_option('hfinsert_header_css'));
+    echo "<textarea name='hfinsert_header_css' rows='5' cols='50' style='width: 100%;'>$header_css</textarea>";
 }
 
-function HFI_header_js_callback() {
-    $header_js = esc_textarea(get_option('HFI_header_js'));
-    echo "<textarea name='HFI_header_js' rows='5' cols='50' style='width: 100%;'>$header_js</textarea>";
+function hfinsert_header_js_callback() {
+    $header_js = esc_textarea(get_option('hfinsert_header_js'));
+    echo "<textarea name='hfinsert_header_js' rows='5' cols='50' style='width: 100%;'>$header_js</textarea>";
 }
 
-function HFI_footer_js_callback() {
-    $footer_js = esc_textarea(get_option('HFI_footer_js'));
-    echo "<textarea name='HFI_footer_js' rows='5' cols='50' style='width: 100%;'>$footer_js</textarea>";
+function hfinsert_footer_js_callback() {
+    $footer_js = esc_textarea(get_option('hfinsert_footer_js'));
+    echo "<textarea name='hfinsert_footer_js' rows='5' cols='50' style='width: 100%;'>$footer_js</textarea>";
 }
 
-// Enqueue custom styles and scripts
-add_action('wp_head', 'HFI_add_custom_css_js_header');
-add_action('wp_footer', 'HFI_add_custom_js_footer');
-
-function HFI_add_custom_css_js_header() {
-    if ($custom_css = get_option('HFI_header_css')) {
-        echo "<style>$custom_css</style>";
+// Enqueue custom styles and scripts in the header
+function hfinsert_add_custom_css_js_header() {
+    $custom_css = get_option('hfinsert_header_css');
+    $custom_js = get_option('hfinsert_header_js');
+    
+    // Add inline CSS
+    if (!empty($custom_css)) {
+        wp_register_style('hfinsert-custom-style', false);
+        wp_enqueue_style('hfinsert-custom-style');
+        wp_add_inline_style('hfinsert-custom-style', wp_strip_all_tags($custom_css));
     }
-    if ($custom_js = get_option('HFI_header_js')) {
-        echo "<script>$custom_js</script>";
+    
+    // Add inline JavaScript
+    if (!empty($custom_js)) {
+        wp_register_script('hfinsert-custom-js-header', false);
+        wp_enqueue_script('hfinsert-custom-js-header');
+        wp_add_inline_script('hfinsert-custom-js-header', wp_kses_post($custom_js));
     }
 }
 
-function HFI_add_custom_js_footer() {
-    if ($custom_js = get_option('HFI_footer_js')) {
-        echo "<script>$custom_js</script>";
+add_action('wp_head', 'hfinsert_add_custom_css_js_header');
+
+// Enqueue custom scripts in the footer
+function hfinsert_add_custom_js_footer() {
+    $custom_js_footer = get_option('hfinsert_footer_js');
+
+    if (!empty($custom_js_footer)) {
+        wp_register_script('hfinsert-custom-js-footer', false);
+        wp_enqueue_script('hfinsert-custom-js-footer');
+        wp_add_inline_script('hfinsert-custom-js-footer', wp_kses_post($custom_js_footer));
     }
 }
+
+add_action('wp_footer', 'hfinsert_add_custom_js_footer');
